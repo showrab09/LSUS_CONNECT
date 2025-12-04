b78867f267f1db8d242f5a7b2651701581c1574f
showrab09 <showrab09@users.noreply.github.com>
2025-12-04 04:07:25 +0000

    deploy: 9161468847842a9628490fb57a02b33126fad2f7

--- _next/static/3lysCIZL5C9uJYPVeTEJo/_buildManifest.js
+++ _next/static/3lysCIZL5C9uJYPVeTEJo/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/undefined//_next/:path+",
-        "destination": "/undefined/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
